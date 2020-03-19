dac98b6bd0eb680de55f51a6fafe9afe3a949734
Yenner Diaz <ydiazn@udg.co.cu>
2020-03-19 13:40:05 -0400

    Documentacion de la vista encarga de visualizar el listado de discusiones
     On branch index-page
     Changes to be committed:
    	modified:   vocationQA/qa/views.py

--- vocationQA/qa/views.py
+++ vocationQA/qa/views.py
@@ -14,10 +14,24 @@ from . import models
 
 
 class IndexView(ListView):
+    '''
+    Listado de las discusiones
+    '''
 
     def get_queryset(self):
+        '''
+        Redefinicion del queryset por defecto
+
+        Se incluye al queryset el campo respuestas__count cuyo valor
+        se corresponde con la cantidad de respuestas que tiene la
+        pregunta asociada a la discusion
+        '''
         queryset = models.Discusion.objects.annotate(
             Count('respuestas'),
         )
+        # Optimizacion: reduccion de las consultas SQL necesarias para
+        # para obtener la informacion de la pregunta aasociada a la discusion.
+        # https://docs.djangoproject.com/en/3.0/ref/models/querysets/#select-related
         queryset = queryset.select_related('pregunta')
+
         return queryset
